45bd0386f697c1db9535309908632bb56dc669bc
Shrimadhav U K <SpEcHIDe@users.noreply.github.com>
2020-07-07 23:19:25 +0530

    add the search api

--- web/config.php
+++ web/config.php
@@ -18,5 +18,56 @@ EOM;
 
 $GLOBALS["CHECKING_MESSAGE"] = "🤔";
 
+$GLOBALS["SAPI_BASE_URL"] = getenv("SAPI_BASE_URL");
+
+$GLOBALS["MESG_DETIDE"] = <<<EOM
+please select your required subtitle
+EOM;
+
+/**
+ * a wraper function to call the search api,
+ * and return a reply_markup containing Telegram Buttons
+ */
+function search_srt_a($s, $p) {
+    // set the search URL with the search query
+    $search_url = $GLOBALS["SAPI_BASE_URL"] . "/search/" . urlencode($s) . "/" . $p . "";
+
+    // get the responses from the API
+    $search_response = json_decode(
+        file_get_contents(
+            $search_url
+        ),
+        true
+    );
+
+    // initialize an empty array
+    // which would store the final "reply_markup"
+    $reply_markup_inline_keyboard_arrey = array();
+
+    foreach ($search_response["r"] as $key => $value) {
+        // this should contain the CAPTION that can be displayed on the button
+        $message_caption = $value["SIQ"];
+        // the file size of the SRT file
+        $file_size = $value["ISF"];
+        // the unique ID of the SRT file, to uniquely identify it
+        $sub_id = $value["DMCA_ID"];
+        // direct download link of the file,
+        // can be empty (at times)
+        $direct_download_link = $value["DLL"];
+
+        $reply_markup_inline_keyboard_arrey[] = array(
+            array(
+                "text" => $message_caption,
+                "callback_data" => "" . "dl_" . $sub_id . ""
+            )      
+        );
+    }
+
+    $reply_markup = json_encode(array(
+        "inline_keyboard" => $reply_markup_inline_keyboard_arrey
+    ));
+    return $reply_markup; 
+}
+
 // import Telegram Bot API libraries
 require_once __DIR__ . "/../vendor/autoload.php";
--- web/index.php
+++ web/index.php
@@ -48,12 +48,31 @@ if (isset($update["message"])) {
         }
 
         else {
+            // send a status message to edit the response,
+            // as required
             $status_message = $bot->api->sendMessage(array(
                 "chat_id" => $chat_id,
                 "text" => $GLOBALS["CHECKING_MESSAGE"],
                 "parse_mode" => "HTML",
                 "disable_notification" => True,
                 "reply_to_message_id" => $message_id
+            ));
+
+            // search subtitles using the API
+            $reply_markup = search_srt_a(
+                $message_text,
+                1
+            );
+
+            // edit the previously sent message,
+            // with the buttons
+            $bot->api->editMessageText(array(
+                "chat_id" => $chat_id,
+                "message_id" => $status_message->message_id,
+                "text" => $GLOBALS["MESG_DETIDE"],
+                "parse_mode" => "HTML",
+                "disable_web_page_preview" => True,
+                "reply_markup" => $reply_markup
             ));
         }
     }
